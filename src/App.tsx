738d2ac6839017153b21c743b9057a7947b50022
Brand0nLe <93brandonle@gmail.com>
2023-05-13 19:30:00 -0700

    import homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-
+import HomePage from './pages/HomePage';
 
 
 function App() {
   return (
     <div> 
-
+      <HomePage/>
     </div>
     );
 }
